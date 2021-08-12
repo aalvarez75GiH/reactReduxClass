ce7b682bb8a16122ee127d4edb67d2fb05382082
Arnoldo Alvarez <p3rolix@Users-iMac.attlocal.net>
2021-08-12 09:49:40 -0400

    2nd commit of project 7

--- project7/src/component/postList.js
+++ project7/src/component/postList.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { fetchPostsAndUsers } from '../actions'
 import UserHeader from './userHeader'
-
+// import { fetchPosts } from '../actions'
 class PostList extends React.Component {
     componentDidMount(){
         this.props.fetchPostsAndUsers()
--- project7/src/component/userHeader.js
+++ project7/src/component/userHeader.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+// import { fetchUser } from '../actions'
 
 class UserHeader extends Component {
+
+    // componentDidMount() {
+    //     this.props.fetchUser(this.props.userId)
+    // }
     
     render() {
   
