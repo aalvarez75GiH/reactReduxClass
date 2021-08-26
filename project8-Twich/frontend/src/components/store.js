1a291b0c3acf4b18222c377454147eabb54e51f5
Arnoldo Alvarez <p3rolix@Users-iMac.attlocal.net>
2021-08-26 09:50:29 -0400

    making commit of api and some changes at frontEnd

--- project8-Twich/frontend/src/components/store.js
+++ project8-Twich/frontend/src/components/store.js
@@ -1,7 +1,24 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import reducers from '../reducers'
+import thunk from 'redux-thunk'
 
 
-const store = createStore(reducers)
+// const composeEnhancers =
+//   typeof window === 'object' &&
+//   window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?   
+//     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+//       // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+//     }) : compose;
+
+// const enhancer = composeEnhancers(
+//   applyMiddleware(),
+//   // other store enhancers if any
+// );
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)))
+
+
+
+
 
 export default store
---
+++ project8-Twich/frontend/src/reducers/streamReducer.js
@@ -0,0 +1,24 @@
+import {
+    SIGN_IN, 
+    SIGN_OUT, 
+    CREATE_STREAM,
+    FETCH_STREAMS, 
+    FETCH_STREAM,
+    EDIT_STREAM,
+    DELETE_STREAM
+
+} from '../actions/types'
+
+const streamReducer = (state = {}, action) => {
+    switch(action.type) {
+        case
+        'CREATE_STREAM':
+        default:
+            return state
+    }
+}
+
+export default streamReducer
+
+
+
